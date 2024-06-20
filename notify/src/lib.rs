7ff23bcbf0199df07774265d633c5e54b3b1878b
Tobias Merkle <tgamerkle@gmail.com>
2024-06-20 16:13:11 -0400

    ui

--- notify/src/lib.rs
+++ notify/src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(let_chains)]
 use crate::kinode::process::notify::{
-    Notification as Notif, Request as NotifyRequest, Response as NotifyResponse,
+    Notification as Notif, ProcessNotifConfig, Request as NotifyRequest, Response as NotifyResponse,
 };
 use kinode_process_lib::{
     await_message, call_init, get_blob, get_typed_state,
@@ -9,7 +9,7 @@ use kinode_process_lib::{
         bind_http_path, send_response, HttpClientAction, HttpServerRequest, Method,
         OutgoingHttpRequest, StatusCode,
     },
-    println, set_state, Address, LazyLoadBlob, ProcessId, Request, Response,
+    println, set_state, Address, LazyLoadBlob, Message, ProcessId, Request, Response,
 };
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
@@ -22,108 +22,120 @@ wit_bindgen::generate!({
 });
 
 #[derive(Serialize, Deserialize)]
-struct ProcessNotifConfig {
-    allow: bool,
-}
-
-#[derive(Serialize, Deserialize)]
 struct NotifState {
-    config: HashMap<ProcessId, ProcessNotifConfig>,
+    config: HashMap<String, ProcessNotifConfig>,
     archive: HashMap<String, Vec<Notif>>,
     push_tokens: Vec<String>,
+}
+
+fn handle_http_request(req: HttpServerRequest, state: &mut NotifState) -> anyhow::Result<()> {
+    match req {
+        HttpServerRequest::Http(incoming) => {
+            let path = incoming.bound_path(Some("notify:notify:sys"));
+            match path {
+                "/add-token" => {
+                    if let Ok(Method::POST) = incoming.method()
+                        && let Some(body) = get_blob()
+                    {
+                        let token: String = serde_json::from_slice(&body.bytes).unwrap();
+                        state.push_tokens.push(token);
+                        set_state(&bincode::serialize(&state)?);
+                        send_response(StatusCode::CREATED, Some(HashMap::new()), vec![]);
+                    } else {
+                        send_response(StatusCode::BAD_REQUEST, Some(HashMap::new()), vec![]);
+                    }
+                }
+                "/notifs" => {
+                    let mut notifs_list: Vec<Notif> = vec![];
+                    for (_process, notifs) in state.archive.iter() {
+                        for notif in notifs {
+                            notifs_list.push(notif.clone());
+                        }
+                    }
+                    send_response(
+                        StatusCode::OK,
+                        Some(HashMap::from([(
+                            "Content-Type".to_string(),
+                            "text/html".to_string(),
+                        )])),
+                        serde_json::to_vec(&notifs_list)?,
+                    );
+                }
+                _ => {
+                    send_response(StatusCode::NOT_FOUND, None, vec![]);
+                }
+            }
+        }
+        _ => {}
+    }
+    Ok(())
+}
+
+fn handle_request(message: Message, our: &Address, state: &mut NotifState) -> anyhow::Result<()> {
+    let body = message.body();
+    let source = message.source();
+    match serde_json::from_slice(body)? {
+        NotifyRequest::Push(ref notif) => {
+            if source.node == our.node {
+                println!("push request: {}", source.process.clone().to_string());
+
+                state
+                    .archive
+                    .entry(source.process.clone().to_string())
+                    .and_modify(|e| e.push(notif.clone()))
+                    .or_insert(vec![notif.clone()]);
+
+                set_state(&bincode::serialize(&state)?);
+
+                if let Some(config) = state.config.get(&source.process.to_string())
+                    && config.allow
+                {
+                    // TODO: send notification
+                }
+            } else {
+                // TODO: ignore notifications from other nodes?
+            }
+            Response::new()
+                .body(serde_json::to_vec(&NotifyResponse::Push).unwrap())
+                .send()
+                .unwrap();
+        }
+        NotifyRequest::History(ref process) => {
+            println!("history request for process: {}", process);
+            Response::new()
+                .body(
+                    serde_json::to_vec(&NotifyResponse::History(
+                        state
+                            .archive
+                            .get(process)
+                            .map(|ns| ns.clone())
+                            .unwrap_or_default(),
+                    ))
+                    .unwrap(),
+                )
+                .send()
+                .unwrap();
+        }
+        NotifyRequest::UpdateSettings(ref config) => {
+            state
+                .config
+                .insert(config.clone().process.to_string(), config.clone());
+            set_state(&bincode::serialize(&state)?);
+        }
+    }
+    Ok(())
 }
 
 fn handle_message(our: &Address, state: &mut NotifState) -> anyhow::Result<()> {
     let message = await_message()?;
 
-    if message.is_request()
-        && message.source().process == "http_server:distro:sys"
-        && let Ok(req) = serde_json::from_slice::<HttpServerRequest>(message.body())
-    {
-        match req {
-            HttpServerRequest::Http(incoming) => {
-                let path = incoming.bound_path(Some("notify:notify:sys"));
-                match path {
-                    "/add-token" => {
-                        if let Ok(Method::POST) = incoming.method()
-                            && let Some(body) = get_blob()
-                        {
-                            let token: String = serde_json::from_slice(&body.bytes).unwrap();
-                            state.push_tokens.push(token);
-                            set_state(&bincode::serialize(&state)?);
-                            send_response(StatusCode::CREATED, Some(HashMap::new()), vec![]);
-                        } else {
-                            send_response(StatusCode::BAD_REQUEST, Some(HashMap::new()), vec![]);
-                        }
-                    }
-                    "/notifs" => {
-                        let mut notifs_list: Vec<Notif> = vec![];
-                        for (_process, notifs) in state.archive.iter() {
-                            for notif in notifs {
-                                notifs_list.push(notif.clone());
-                            }
-                        }
-                        send_response(
-                            StatusCode::OK,
-                            Some(HashMap::from([(
-                                "Content-Type".to_string(),
-                                "text/html".to_string(),
-                            )])),
-                            serde_json::to_vec(&notifs_list)?,
-                        );
-                    }
-                    _ => {
-                        send_response(StatusCode::NOT_FOUND, None, vec![]);
-                    }
-                }
-            }
-            _ => {}
-        }
-    } else {
-        let body = message.body();
-        let source = message.source();
-        match serde_json::from_slice(body)? {
-            NotifyRequest::Push(ref notif) => {
-                if source.node == our.node {
-                    println!("push request: {}", source.process.clone().to_string());
-
-                    state
-                        .archive
-                        .entry(source.process.clone().to_string())
-                        .and_modify(|e| e.push(notif.clone()))
-                        .or_insert(vec![notif.clone()]);
-
-                    set_state(&bincode::serialize(&state)?);
-
-                    if let Some(config) = state.config.get(&source.process)
-                        && config.allow
-                    {
-                        // TODO: send notification
-                    }
-                } else {
-                    // TODO: ignore notifications from other nodes?
-                }
-                Response::new()
-                    .body(serde_json::to_vec(&NotifyResponse::Push).unwrap())
-                    .send()
-                    .unwrap();
-            }
-            NotifyRequest::History(ref process) => {
-                println!("history request for process: {}", process);
-                Response::new()
-                    .body(
-                        serde_json::to_vec(&NotifyResponse::History(
-                            state
-                                .archive
-                                .get(process)
-                                .map(|ns| ns.clone())
-                                .unwrap_or_default(),
-                        ))
-                        .unwrap(),
-                    )
-                    .send()
-                    .unwrap();
-            }
+    if message.is_request() {
+        if message.source().process == "http_server:distro:sys"
+            && let Ok(req) = serde_json::from_slice::<HttpServerRequest>(message.body())
+        {
+            handle_http_request(req, state)?;
+        } else {
+            handle_request(message, our, state)?;
         }
     }
     Ok(())
